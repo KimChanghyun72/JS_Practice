aa6698dd170bdca544bb9931f722470edffbe587
Your Name <ok2qkq@naver.com>
2020-05-16 15:57:15 +0900

    js_varible

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-alert('I am Working. Im JS. Im Beautiful. Im worth it');
-console.log('I am Working. Im JS. Im Beautiful. Im dark soul');
+// //instead let, use const, you can't change varible
+// let a = 221;
+// const b = a - 5;
+// a=4;
+// console.log(b);
+
+//String
+const what = "Nicolas";
+console.log(what);
